4a94f2e1a1b7b7e8b6be191314863fa871a32902
Lucas Silva <lucasfic2017@gmail.com>
2022-09-18 03:11:41 -0300

    exercícios do dia 7.1

---
+++ 01-fundamentos/Seção-07-introdução-a-javascript-es6-e-testes-unitários/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/snipets/scripts.js
@@ -0,0 +1,142 @@
+// VAR, LET E CONST
+function userInfo() {
+  let userEmail = 'maria@email.com';
+
+  // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
+  console.log(userEmail);
+}
+userInfo();
+
+if (true) {
+  // inicio do escopo do if
+  var userAge = '20';
+  console.log(userAge); // 20
+  // fim do escopo do if
+}
+console.log(userAge); // 20
+
+var userName = 'Isabella';
+var userName = 'Lucas';
+console.log(userName); // Resultado: Lucas
+
+const favoriteLanguage = 'Javascript';
+favoriteLanguage = 'Python';
+console.log(favoriteLanguage); // Erro
+
+let favoriteTechnology = 'Machine learning';
+favoriteTechnology = 'Facial recognition';
+console.log(favoriteTechnology); // Facial recognition
+
+const userInfo = {
+  name: 'Cláudio',
+  id: '5489-2',
+  email: 'claudio@email.com',
+};
+
+userInfo.name = 'João';
+
+console.log(userInfo); // { name: 'João', id: '5489-2', email: 'claudio@email.com' }
+
+const technologies = ['Javascript', 'CSS', 'HTML'];
+technologies.push('React');
+console.log(technologies); // [ 'Javascript', 'CSS', 'HTML', 'React' ]
+
+technologies = ['Javascript', 'CSS', 'HTML', 'React']
+console.log(technologies); // Erro
+
+// HOISTING
+
+age = 20;
+
+var age;
+
+console.log(age); // 20
+
+var age;
+
+age = 20;
+
+console.log(age); // 20
+
+var age;
+
+console.log(age); // undefined
+
+age = 20;
+
+// Template Literals
+
+const myName = 'Isabella';
+console.log('Hello' + ' ' + myName + '!');
+
+// const myName = 'Isabella';
+// console.log(`Welcome ${myName}!`);
+
+// Com o template literals
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`
+);
+
+// Sem o template literals:
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
+
+// Arrow functions
+const printName = function () {
+  const myName = 'Lucas';
+  return myName;
+};
+
+console.log(printName());
+
+// const printName = () => {
+// const myName = 'Lucas';
+// return myName;
+// };
+
+console.log(printName());
+
+// const printName = () => 'Lucas';
+// console.log(printName());
+
+const multiplyByTwo = number => number * 2;
+console.log(multiplyByTwo(10));
+
+const multiplication = (number, multi) => number * multi;
+console.log(multiplication(8, 2));
+
+// ternary operator
+
+// Situação em que é mais simples usar o operador ternário:
+// const checkIfElse = (age) => {
+//   if (age >= 18) {
+//     return `Você tem idade para dirigir!`;
+//   } else {
+//     return `Você ainda não tem idade para dirigir...`;
+//   }
+// };
+
+// const checkTernary = (age) => (
+//   age >= 18 ? `Você tem idade para dirigir!` : `Você ainda não tem idade para dirigir...`;
+// );
+
+// // ------------------------
+
+// // Situação em que usar o operador ternário não faz muito sentido:
+// const checkIfElse = (fruit) => {
+//   if (fruit === `maçã`) {
+//     return `Essa fruta é vermelha`;
+//   } else if (fruit === `banana`) {
+//     return `Esta fruta é amarela`;
+//   } else {
+//     return `Não sei a cor`;
+//   }
+// };
+
+// const checkTernary = (fruit === `maçã`) ? `Essa fruta é vermelha` 
+//   : ((fruit === `banana`) ? `Esta fruta é amarela` : `Não sei a cor`);
+
+// Aninhar operadores  ternários, como no exemplo acima, não é uma boa prática,
+// pois torna o seu código truncado e difícil de ler, tirando todo o sentido de um
+// operador cujo objetivo é facilitar a sua vida e a da pessoa que lerá seu código
+// no futuro!
